48938f8cf87dc4bc020c64cf4f243053b163370e
SofiaHjerpe <hjerpesofia@gmail.com>
2023-09-20 12:18:58 +0200

    Update vite.config.js

--- MyWebsiteInVue/vite.config.js
+++ MyWebsiteInVue/vite.config.js
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sofiahjerpe-delploying-vite/',
   plugins: [vue()],
   resolve: {
     alias: {
